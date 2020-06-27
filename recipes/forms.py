e2ec15148221a9f6c361d844d08400985923db34
Kathy Tran <44452243+katran009@users.noreply.github.com>
2020-06-26 19:50:26 -0500

    Add files via upload

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from recipes.models import Author
 
-class NewsAddForm(forms.Form):
+
+class RecipesAddForm(forms.Form):
     title = forms.CharField(max_length=30)
     description = forms.CharField(widget=forms.Textarea)
     author = forms.ModelChoiceField(queryset=Author.objects.all())
